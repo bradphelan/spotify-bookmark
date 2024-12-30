5303aaedc76ae82c3c2ceae0667e854f53a2b8e6
brad phelan <bradphelan@xtargets.com>
2024-12-31 00:36:48 +0100

    Refactor Spotify redirect URI logic to use import.meta.env.MODE for environment detection

--- src/lib/stores/spotify.ts
+++ src/lib/stores/spotify.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 import { SpotifyApi, type AccessToken } from '@spotify/web-api-ts-sdk';
+const mode = import.meta.env.MODE;
 
 export const spotifyPlayer = writable<SpotifyApi>();
 export const isPlayerReady = writable(false);
@@ -8,7 +9,7 @@ const clientId = "b6e8a090fcd3474ea29f034b6d3bf81c"
 const spotifyStorageKey = "spotify-sdk:AuthorizationCodeWithPKCEStrategy:token"
 const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'streaming']
 
-const redirectUri = process.env.NODE_ENV == 'production' ?
+const redirectUri = mode === 'production' ?
  'https://bradphelan.github.io/spotify-bookmark/' :
  'http://localhost:5173';
 
