57c07147288d81d72b2a894424b6a8143035f9d7
brad phelan <bradphelan@xtargets.com>
2024-12-31 00:27:35 +0100

    Update Spotify redirect URI for production and development environments

--- src/lib/stores/spotify.ts
+++ src/lib/stores/spotify.ts
@@ -8,7 +8,9 @@ const clientId = "b6e8a090fcd3474ea29f034b6d3bf81c"
 const spotifyStorageKey = "spotify-sdk:AuthorizationCodeWithPKCEStrategy:token"
 const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'streaming']
 
-const redirectUri = 'http://localhost:5173';
+const redirectUri = process.env.NODE_ENV == 'production' ?
+ 'https://bradphelan.github.io/spotify-bookmark/' :
+ 'http://localhost:5173';
 
 export const authSpotify = () => {
     SpotifyApi.performUserAuthorization(clientId, redirectUri, scopes, async (token: AccessToken) => {
